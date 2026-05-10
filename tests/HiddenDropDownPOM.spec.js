a6d9722bb668012996087483777dfca12d8c5d5f
SamanSoft <ssamad44@gmail.com>
2026-05-10 23:31:51 +1200

    for GitHub integration Test

--- tests/HiddenDropDownPOM.spec.js
+++ tests/HiddenDropDownPOM.spec.js
@@ -21,6 +21,11 @@ test.setTimeout(80000);
 });
     test("UAT TEST @UAT", async({page})=>{
         await page.goto('https://www.saucedemo.com/');
+        await page.locator('#user-name').fill('standard_user');
+        await page.locator('#password').fill('secret_sauce');
+        await page.locator('#login-button').click();
+        await page.waitForTimeout(3000);
+        await expect(page.locator('.title')).toHaveText('Products');
 
         
 });
